9daf8333beec884ccdc9f8ddbda0707e06bfc884
mshagerdi <m.shagerdi@yahoo.com>
2014-04-20 15:40:45 +0430

    Create index.php

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<body dir="rtl">
+<align="justify">
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title> گالري طلا و جواهر زرکده </title>
+<meta name="keywords" content="طلا, جواهر, فروش آنلاين, gold, Zarkadeh, زرکده, ecommerce, online shopping, store" />
+<meta name="description" content="Zarkadeh gold and jewelry is website for ecommerce or online gold shopping websites. Products, Shopping Cart and Checkout pages are included." />
+<link href="templatemo_style.css" rel="stylesheet" type="text/css" />
+
+<link rel="stylesheet" href="css/orman.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+
+<link rel="stylesheet" type="text/css" href="css/ddsmoothmenu.css" />
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/ddsmoothmenu.js">
+
+/***********************************************
+* Smooth Navigational Menu- (c) Dynamic Drive DHTML code library (www.dynamicdrive.com)
+* This notice MUST stay intact for legal use
+* Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
+***********************************************/
+
+</script>
+
+ <?php include 'formula.php'; ?>
+
+<script type="text/javascript">
+
+ddsmoothmenu.init({
+	mainmenuid: "templatemo_menu", //menu DIV id
+	orientation: 'h', //Horizontal or vertical menu: Set to "h" or "v"
+	classname: 'ddsmoothmenu', //class added to menu's outer DIV
+	//customtheme: ["#1c5a80", "#18374a"],
+	contentsource: "markup" //"markup" or ["container_id", "path_to_menu_file"]
+})
+
+function clearText(field)
+{
+    if (field.defaultValue == field.value) field.value = '';
+    else if (field.value == '') field.value = field.defaultValue;
+}
+
+</script>
+
+<link rel="stylesheet" href="css/slimbox2.css" type="text/css" media="screen" /> 
+<script type="text/JavaScript" src="js/slimbox2.js"></script> 
+
+
+</head>
+
+<body>
+
+<div id="templatemo_wrapper_h">
+<div id="templatemo_header_wh">
+	<div id="templatemo_header" class="header_home">
+
+<?php include 'menu.php'; ?>
+
+
+        <div class="slider-wrapper theme-orman">
+            
+            <div id="slider" class="nivoSlider">
+                <img src="images/portfolio/01.jpg" alt="slider image 1" />
+                <img src="images/portfolio/02.jpg" alt="slider image 2" />
+                <img src="images/portfolio/03.jpg" alt="slider image 3" />
+                <img src="images/portfolio/04.jpg" alt="slider image 4" />
+                <img src="images/portfolio/05.jpg" alt="slider image 5" />
+            </div>
+            <div id="htmlcaption" class="nivo-html-caption">
+                
+            </div>
+        </div> 
+		<script type="text/javascript" src="js/jquery-1.6.1.min.js"></script>
+        <script type="text/javascript" src="js/jquery.nivo.slider.pack.js"></script>
+        <script type="text/javascript">
+        $(window).load(function() {
+            $('#slider').nivoSlider({
+				controlNav:false
+			});
+        });
+        </script>
+    </div> <!-- END of header -->
+</div> <!-- END of header wrapper -->
+<div id="templatemo_main_wrapper">
+<div id="templatemo_main">
+ <?php include 'slidebar.php'; ?>
+    </div>
+ 
+    <div id="content" class="right">
+		<h2>به زرکده خوش آمديد</h2>
+		<p>اين سايت در صدد است تا خريدي امن و مطمئن را براي شما فراهم آورد</a>.<BR/>
+		توجه داشته باشید که تمام قیمت های درج شده در سایت به صورت آنلاین با قیمت طلا در اتحادیه طلا و جواهر هماهنگ بوده و با تغییر قیمت مرجع به صورت آنلاین تغییر می کند. <br/>
+		لازم به ذکر است که در هنگامی که قیمت طلا در سایت اتحادیه آپدیت نمی شود (روز های تعطیل و در ساعت تعطیلی بازار) قیمت ها از سایت حذف می گردند و شما باید تا قرار گرفتن دوباره قیمت ما را با صبر خود یاری رسانید. <br/>
+		کالاهای فروخته شده از طریق کد آنها قابل سفارش دهی هستند.<br/>
+		با توجه به نوسانات بازار جهانی و قیمت ارز از دادن اجناس به صورت اقساط معذوریم.<br/>
+		نحوه ی محاسبه ی قیمت: (قیمت هر گرم طلا به ریال + اجرت: دستمزد ساخت + 8% مالیات بر ارزش افزوده + 7% سود)* وزن کالا<br/><br/>
+		
+		* هر گونه کپی برداری از سایت و یا استفاده بدون هماهنگی از محتویات آن پیگرد قانونی دارد.<br/>
+		
+		
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+</p>
+        
+
+
+        <div class="blank_box">
+        	<?php include 'main_footer.php'; ?>
+        </div>    
+    </div>
+    <div class="cleaner"></div>
+</div> <!-- END of main -->
+</div> <!-- END of main wrapper -->
+
+<div id="templatemo_footer_wrapper">
+<div id="templatemo_footer">
+<?php include 'footer.php'; ?>
+</div> <!-- END of footer -->
+</div> <!-- END of footer wrapper -->
+</div>
+
+</body>
+<script type='text/javascript' src='js/logging.js'></script>
+</html>
